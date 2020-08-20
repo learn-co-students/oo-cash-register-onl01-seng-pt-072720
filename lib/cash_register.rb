9d51be42a7b8b57a3e6bcf28a1ff76f3179939e3
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 00:58:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :items_with_prices
 
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+      @price = price
+      @total = @total + (price * quantity)
+      @items ||= []
+      quantity.times do
+        @items << title
+      end
+
+      @last_quantity_bought = quantity
+      @last_price = price # yes, we already have a reference to this as @price, but this is just setting a new variable explicitly for our use case
+    end
+
+    def void_last_transaction
+      ## now, down here, when we invoke this
+
+      total_last_transaction = @last_quantity_bought * @last_price
+      @total = @total - total_last_transaction
+
+      ## we can subtract the total of the last transaction.
+
+      @last_quantity_bought.times do
+        @items.pop
+        ## pop an item out
+      end
+    end
+
+  def apply_discount
+    if discount
+      self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+end
