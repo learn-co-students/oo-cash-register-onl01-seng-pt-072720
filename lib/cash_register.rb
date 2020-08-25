5a5c70aeff01e5597bf86ae87e866e011f6a86d9
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-25 13:43:10 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,38 +4,39 @@ class CashRegister
 
 attr_accessor :total, :discount, :title, :price, :last_transaction, :items
 
-def initialize(discount=0)
-  @discount = discount  
-  @total = 0 
-  @items = []
-end 
+  def initialize(discount=0)
+    @discount = discount  
+    @total = 0 
+    @items = []
+  end 
   
-def add_item(title, price, quantity = 1)
-  if quantity < 1 
-    @total += price
-  else 
-    @total += (quantity*price)
-    @last_transaction = @total
-    @total 
-    
-    @items << title 
+  def add_item(title, price, quantity = 1)
+      @last_transaction = price*quantity
+      @total += @last_transaction
+      if quantity < 1 
+      @items << title 
+    else 
+      quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << title
+        counter += 1
+      end
+    end 
   end 
-end 
 
-def apply_discount()
-  if @discount > 0 
-    @discount = @discount/100.to_f
-    @total = @total-(@total*@discount)
-    puts "After the discount, the total comes to $#{@total}"
-  else 
-    puts "There is no discount to apply."
+  def apply_discount()
+    if @discount > 0 
+      @discount_percentage = @discount/100.to_f
+      @total -= (@total*@discount_percentage).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else 
+      "There is no discount to apply."
+    end 
   end 
-end 
 
-def void_last_transaction
-  @total -= @last_transaction
-  # if @items = @items.empty? 
-  #   @total = [] 
-end
+  def void_last_transaction
+    @total -= @last_transaction
+  end 
 
 end
